cdd3e535bd13a687eac8225f2682b3bdaff44757
Otger Ballester <otger@ifae.es>
2017-04-18 17:42:29 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
             name="pytticpx",
-            packages = ['pytticpx'],
-            version="0.0.1",
+            packages=['pytticpx'],
+            version="0.1.1",
             description="Library to communicate with TTi CPX power supply",
             author="David Roman",
             author_email="droman@ifae.es",
-            url = 'https://gitlab.pic.es/ifaecontrol/pytticpx',
+            url='https://gitlab.pic.es/ifaecontrol/pytticpx',
         )
