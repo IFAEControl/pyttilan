56b1204d6041a5b7898d696a068b7dc5e3abcf46
Otger Ballester <otger@ifae.es>
2025-11-20 17:48:46 +0100

    Version 0.2.2 Updated gitignore with ide folders Updated setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Minimal shim setup.py for legacy tooling.
 
-import os
-from setuptools import setup, find_packages
+Metadata is provided in `pyproject.toml` (PEP 621). This file exists to
+allow older tools that still import `setup.py` to work. Modern build tools
+use PEP 517/518 and will read metadata from `pyproject.toml`.
+"""
+from setuptools import setup
 
-# Read README and CHANGES files for the long description
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as fh:
-      long_description = fh.read()
-
-print(find_packages(exclude=('tests',)))
-setup(
-      name="pyttilan",
-      version="0.2.1",
-      description="Library to control TTi Power Supplies over network",
-      long_description_content_type="text/markdown",
-      long_description=long_description,
-      requires=[],
-      install_requires=[],
-      provides=["pyttilan"],
-      author="David Roman, Otger Ballester",
-      author_email="ifae-control@ifae.es",
-      license="CC0 1.0 Universal",
-      url="https://github.com/IFAEControl/pyttilan",
-      zip_safe=False,
-      classifiers=[
-                   "Development Status :: 4 - Beta",
-                   "Programming Language :: Python :: 3",
-                  ],
-      package_dir={'': 'src'},
-      packages=find_packages(where=os.path.join('.', 'src'), exclude=('tests',))
-)
+# Delegate metadata to `pyproject.toml` (PEP 517). Keeping a minimal
+# setup.py helps compatibility with some tools and environments.
+setup()
